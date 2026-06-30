e9bbd082dcf6652dd9f4b4594a1b4639b117cc27
JyotiRawat29 <53561917+JyotiRawat29@users.noreply.github.com>
2026-07-01 02:18:47 +0530

    oops

---
+++ strings/gcdOfStringsOOP.py
@@ -0,0 +1,17 @@
+class solution:
+    @staticmethod
+    def findgcd(a,b):
+        if b == 0:
+            return a
+        return solution.findgcd(b,a%b)
+    
+    @staticmethod
+    def gcdOfStrings(str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+           return ""
+        prefix = solution.findgcd(max(len(str1), len(str2)), min(len(str1),len(str2)))
+        candidate = str1[:prefix]
+        if (str1 == candidate * (len(str1)//prefix)) and (str2 == candidate * (len(str2)//prefix)):
+            return candidate
+        else:
+            return ""
